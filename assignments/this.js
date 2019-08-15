04b5b5fb99addb80c3f9367425b5c9ba5d38308a
Shafi Masoumi <masoumi2017@icloud.com>
2019-08-14 18:01:48 -0700

    Explaince added for principles of This

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window or global Binding: When this "this" keyword is called in the global scope, and is not bound to anything specific, it is considered Window Bound.
+* 2. Implicit - To the left! With implicit bindins you can always look to the left of the dot (.) when a method is called.
+* 3. New Binding - When invoking a new object with the "this" keyword, that is considered a new binding.
+* 4. Explicit - Just like it sounds, when you explicitly give the this keyword a value, that is considered explicit binding.
 *
 * write out a code example of each explanation above
 */
@@ -19,7 +19,7 @@
 
 // Principle 3
 
-// code example for New Binding
+// code example for New Binding 
 
 // Principle 4
 
